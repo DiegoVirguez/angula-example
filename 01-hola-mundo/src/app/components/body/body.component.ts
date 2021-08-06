1bf02f98247a996eb6b80244f29a606e185314c6
diego-virguez <915+diego-virguez@users.noreply.github.xpo.com>
2021-08-06 10:12:47 -0500

    first commit

---
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-body',
+    templateUrl: './body.component.html'
+})
+export class BodyComponent{
+
+    mostrar = true;
+
+    frase: any ={
+        mensaje: 'Un gran poder requiere una gran responsabilidad',
+        autor: 'Ben Parker'
+    };
+
+    //el ngFor trabaja en base a arreglos
+
+    personajes: string[] = ['Spiderman', 'Venom', 'Dr. Octopus'];
+
+}
